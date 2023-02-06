5b6b4948292277a911e6f3dfbbca3ce738b01237
sayyidusy <sayyidusyalghiffari@gmail.com>
2023-02-07 01:17:36 +0700

    learnSvelte

---
+++ Loading-Data/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="sm:mx-auto sm:w-full sm:max-w-md">
+	<div class="flex flex-col gap-4 items-center">
+		<h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 bg-sky-400">
+			Form Actions in SvelteKit
+		</h2>
+		<a href="/contacts" class="btn btn-primary max-w-xs">Contacts</a>
+	</div>
+</div>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
